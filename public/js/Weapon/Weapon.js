a6369d7680df2854c236510721bc8823b0a1da0d
luniksss <nikolaeval316@gmail.com>
2024-06-26 17:09:13 +0300

    заменила все циклы на map, var на const,  навела красоту

--- public/js/Weapon/Weapon.js
+++ public/js/Weapon/Weapon.js
@@ -1,9 +1,13 @@
 import { WEAPON_SET } from "../settings.js";
 import { CELL_SET } from "../settings.js";
 
+const state = {
+    onTheGround: 0, 
+    inTheHand: 1,
+}
 
 class Weapon {
-    constructor(name, x, y, battleType, rapidity, grouping, deviation, status, onGround, inHand, context) {
+    constructor({name, x, y, battleType, rapidity, grouping, deviation, onGround, inHand}, context) {
         this.name = name;
         this.x = (x * CELL_SET.w) + (CELL_SET.w * 0.5);
         this.y = (y * CELL_SET.h) + (CELL_SET.h * 0.5);
@@ -11,20 +15,20 @@ class Weapon {
         this.rapidity = rapidity;
         this.grouping = grouping;
         this.deviation = deviation;
-        this.status = status;   //0 - на земле, 1 - в руках
         this.onGround = onGround;   
         this.inHand = inHand;
         this.context = context;
+        this.status = state.onTheGround; 
     }
    
     draw() {
-        if (this.status === 0) {
+        if (this.status === state.onTheGround) {
             const weaponX = this.x - (WEAPON_SET.w * 0.5);
             const weaponY = this.y - (WEAPON_SET.h * 0.5);
             this.context.fillStyle = this.onGround;
             this.context.fillRect(weaponX, weaponY, WEAPON_SET.w, WEAPON_SET.h);   
         }
-        if (this.status === 1) {
+        if (this.status === state.inTheHand) {
             const weaponX = this.x - (WEAPON_SET.w * 0.5);
             const weaponY = this.y - (WEAPON_SET.h * 0.5);
             this.context.fillStyle = this.inHand;
--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,7 @@
 import { WINDOW, CAMERA } from "./settings.js";
 import { BattleGround } from "./BattleGround/BattleGround.js";
 import { Player } from "./Player/Player.js";
-var canvas = document.getElementById("canvas");
+const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 const cellsList = [
     '1111111011', 
@@ -19,10 +19,26 @@ const cellsList = [
     '0110111001', 
     '1110101111'];
 const wallList = [[2, 0, 2, 1], [1, 4, 1, 5], [2, 5, 3, 5], [3, 8, 3, 9], [5, 2, 5, 3], [5, 6, 5, 5], [5, 8, 4, 8], [6, 4, 7, 4], [8, 8, 8, 9], [8, 2, 9, 2], [9, 4, 9, 3], [10, 6, 11, 6], [12, 2, 13, 2], [12, 5, 12, 6]];
-var weaponSet = [
-    ["wep1", 3, 2, "close", 10, 10, 10, 0, "pink", "red"], 
-    ["wep2", 10, 5, "close", 10, 10, 10, 1, "pink", "red"]];  //0 - на земле, 1 - в руках
-var groundList = [];
+const weaponSet = [
+    {name: "wep1", 
+        x: 3, 
+        y: 2, 
+        battleType: "close",  //ближний или дальний бой
+        rapidity: 10,           //скорострельность
+        grouping: 10,           //кучность
+        deviation: 10,          //отклонение
+        onGround: "pink",       //текстура на земле
+        inHand: "red"},         //текстура в руках
+    {name: "wep2",
+        x: 10,
+        y: 5,
+        battleType: "close",
+        rapidity: 10,
+        grouping: 10,
+        deviation: 10,
+        onGround: "pink",
+        inHand: "red"}];
+const groundList = [];
 
 function convertFields(cellsList) {
     for (let i = 0; i < cellsList.length; i++) {
@@ -39,14 +55,14 @@ canvas.width = WINDOW.w;
 canvas.height = WINDOW.h;
 context.fillStyle = WINDOW.c;
 context.fillRect(0, 0, WINDOW.w, WINDOW.h);
-let field = new BattleGround(groundList, wallList, weaponSet, context);
+const field = new BattleGround(groundList, wallList, weaponSet, context);
 
-let player = new Player(1000, 1000, 0, context);
+const player = new Player(1000, 1000, 0, context);
 
 play();
 
 function moveFrame() {
-    let [dx, dy] = [CAMERA.center.x - player.x, CAMERA.center.y - player.y];
+    const [dx, dy] = [CAMERA.center.x - player.x, CAMERA.center.y - player.y];
     const period = (Math.abs(dx) + Math.abs(dy) < 0.5) ? 1 : CAMERA.period;
     field.move(dx / period, dy / period);
     player.move(dx / period, dy / period)
@@ -56,6 +72,7 @@ function drawFrame() {
     field.drawGround();
     player.draw();
     field.drawWalls();
+    field.drawWeapons();
 }
 
 function play() {
