f002f23a41c52fe082fb6e4a95f56dd512197493
luniksss <nikolaeval316@gmail.com>
2024-07-08 15:24:41 +0300

    типы оружий в константах

--- public/js/game/Engine/Weapon/WeaponModel.js
+++ public/js/game/Engine/Weapon/WeaponModel.js
@@ -1,24 +1,24 @@
-import { WEAPON, CELL, WEAPON_STATE } from "../../CONST.js";
+import { CELL, WEAPON_STATE } from "../../CONST.js";
 import { Drawable } from "../Interface/Drawable.js";
 
 class WeaponModel extends Drawable {
-    constructor({name, x, y, battleType, rapidity, grouping, deviation, status, onGround, inHand, amount, rechargeTime}) {
-        super(x * CELL.w + CELL.w * 0.5, y * CELL.h + CELL.h * 0.5, WEAPON.w, WEAPON.h)
-        this.name = name;
-        this.battleType = battleType;
-        this.rapidity = rapidity;
-        this.grouping = grouping;
-        this.deviation = deviation;
+    constructor({id, type, x, y}) {
+        super(x * CELL.w + CELL.w * 0.5, y * CELL.h + CELL.h * 0.5, type.w, type.h)
+        this.name = type.name;
+        this.battleType = type.battleType;
+        this.rapidity = type.rapidity;
+        this.grouping = type.grouping;
+        this.deviation = type.deviation;
         this.onGround = new Image();
-        this.onGround.src = onGround;
+        this.onGround.src = type.onGround;
         this.inHand = new Image();
-        this.inHand.src = inHand;
+        this.inHand.src = type.inHand;
         this.status = WEAPON_STATE.onTheGround; 
         this.shootingInterval;
-        this.amount = amount;
-        this.maxAmount = amount;
+        this.amount = type.amount;
+        this.maxAmount = type.amount;
         this.isRecharging = false;
-        this.rechargeTime = rechargeTime;
+        this.rechargeTime = type.rechargeTime;
     }
 }
 
