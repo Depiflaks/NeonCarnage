afd6f07e61b7431ed386c61ad86a46427cfe8841
luniksss <nikolaeval316@gmail.com>
2024-07-11 11:11:55 +0300

    оружия отрисовываются правильно, чуть поменяла skin model и радиус подбора оружия

--- public/js/game/Engine/Entity/Skin/SkinModel.js
+++ public/js/game/Engine/Entity/Skin/SkinModel.js
@@ -2,18 +2,16 @@ import { SKINS } from "../../../CONST.js";
 
 class SkinModel {
     constructor({ skinId }) {
-        this.image = {
-            body: new Image(),
-            
+        this.head = new Image(),
+        this.body = {
+            none: new Image(),
+            one: new Image(),
+            two: new Image(),
         }
-        this.headImage = new Image();
-        this.bodyImage = new Image();
-        this.bodyWithWeaponImage = new Image();
-        this.bodyWithPistolsImage = new Image();
-        this.headImage.src = SKINS[skinId].head;
-        this.bodyImage.src = SKINS[skinId].body;
-        this.bodyWithWeaponImage.src = SKINS[skinId].bodyWithWeapon;
-        this.bodyWithPistolsImage.src = SKINS[skinId].bodyWithPistols;
+        this.head.src = SKINS[skinId].head;
+        this.body.none.src = SKINS[skinId].body;
+        this.body.one.src = SKINS[skinId].bodyWithPistols;
+        this.body.two.src = SKINS[skinId].bodyWithWeapon;
     }
 }
 
--- public/js/game/Engine/Weapon/WeaponView.js
+++ public/js/game/Engine/Weapon/WeaponView.js
@@ -6,25 +6,22 @@ class WeaponView {
 
     draw(weapon, entities, context) {
         if (weapon.getStatus() === WEAPON_STATE.onTheGround) {
-            //console.log(1);
             this.drawOnGround(weapon, context);  
         }
         if (weapon.getStatus() === WEAPON_STATE.inTheHand) {
-            //console.log(2);
             this.drawInHand(weapon, entities, context);
         }
     }
 
     drawOnGround(weapon, context) {
         const {x, y} = weapon.getPosition();
-        context.drawImage(weapon.getOnGround(), x, y);
+        context.drawImage(weapon.getOnGround(), x - CELL.h / 2, y - CELL.h / 4);
     }
 
     drawInHand(weapon, entities, context) {
         // доделать траекторию, когда начнём получать её с бэкенда
         //if (animation && animation.isAnimating) return;
         for (const entity of entities) {
-            //console.log(entity, entity.getWeaponId(), weapon.getId());
             if (entity.getWeaponId() !== weapon.getId()) continue;
             const {x: px, y: py} = entity.getPosition();
             const {weaponX, weaponY} = this.getParam(weapon);
@@ -32,7 +29,7 @@ class WeaponView {
             context.save();
             context.translate(px, py);
             context.rotate(entity.getAngle() + 90 * RAD);
-            context.drawImage(weapon.getInHand(), weaponX, weaponY, weapon.model.w, weapon.model.h);
+            context.drawImage(weapon.getInHand(), weaponX, weaponY, weapon.getWidth(), weapon.getHeight());
             context.restore();
         }
     }
@@ -40,13 +37,13 @@ class WeaponView {
     getParam(weapon) {
         switch (weapon.getName()) {
             case "knife":
-                return {weaponX: ENTITY.h, weaponY: -weapon.model.inHand.height / 2}
+                return {weaponX: ENTITY.h, weaponY: -weapon.getInHand().height / 2}
             case "glock":
             case "pistol":
-                return {weaponX: -ENTITY.h / 5, weaponY: -weapon.model.inHand.height};
+                return {weaponX: -ENTITY.h / 5, weaponY: -weapon.getInHand().height};
             case "machineGun":
             case "rifle":
-                return {weaponX: -ENTITY.h / 2, weaponY: -weapon.model.inHand.height}
+                return {weaponX: -ENTITY.h / 2, weaponY: -weapon.getInHand().height}
         }
     }
 }
