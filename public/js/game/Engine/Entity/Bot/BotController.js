20bb30cc7583cdb2dfc484b6434f88f549ddd978
rxeside <114814244+rxeside@users.noreply.github.com>
2024-07-22 19:03:47 +0300

    создается экземпляр бота)

--- public/js/game/Engine/Entity/Bot/BotController.js
+++ public/js/game/Engine/Entity/Bot/BotController.js
@@ -4,7 +4,6 @@ import { BotModel } from "./BotModel.js"
 class BotController extends EnemyController {
     constructor(player) {
         super(player)
-        console.log('che')
         this.model = new BotModel(player);
 
     }
--- public/js/game/Engine/Entity/Bot/BotModel.js
+++ public/js/game/Engine/Entity/Bot/BotModel.js
@@ -3,7 +3,7 @@ import { EnemyModel } from "../Enemy/EnemyModel.js";
 class BotModel extends EnemyModel {
     constructor({ x, y, angle, skinId }) {
         super({ x, y, angle, skinId });
-
+        console.log(this)
     }
 }
 
