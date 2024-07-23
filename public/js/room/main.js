65e0b545c32cc8c47ec82ebdd21c3842a7be9cfb
ignatatylov <124980883+ignatatylov@users.noreply.github.com>
2024-07-23 17:05:45 +0300

    добавил звуки

--- public/js/room/main.js
+++ public/js/room/main.js
@@ -2,3 +2,8 @@ import { Room } from "./Room/Room.js";
 
 
 const room = new Room();
+
+const track = new Audio("../../../public/sound/M_O_O_N - Dust.mp3");
+track.loop = false;
+track.volume = 1.0;
+track.play();
--- server/CONST/SERVER/SERVER.js
+++ server/CONST/SERVER/SERVER.js
@@ -15,6 +15,12 @@ export const DATA_BASE = {
         user: "smmm",
         database: "NeonCarnage",
         password: "3030"
+    },
+    ignat: {
+        host: "localhost",
+        user: "root",
+        database: "NeonCarnage",
+        password: "1111"
     }
 }
 
